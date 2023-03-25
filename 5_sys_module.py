a5397b8a8efa983a275671f6341fde56fc8c9496
hasannasrul <hasanali242424@gmail.com>
2023-03-25 05:40:45 +0530

    sys module added

---
+++ 5_sys_module.py
@@ -0,0 +1,30 @@
+import sys
+
+'''
+>>  Note: It has nothing to do with your nderlting OS. It is used to manipulate 
+          your Python runtime environment.
+          important function below
+            sys.version
+            sys.path
+            sys.platform
+            sys.argv
+            sys.modules
+            sys.exit()
+'''
+# get python version
+print(sys.version)
+
+# python will find its executable from here. just like linux uses echo $PATH
+print(sys.path)
+
+# get platform detail. in mac ot will get me kernel name which is darwin
+print(sys.platform)
+
+# get arguments from command ine
+print(sys.argv)
+
+# get all modules which is used by current python environment
+print(sys.modules)
+
+# exiting from current python module
+print(sys.exit())
