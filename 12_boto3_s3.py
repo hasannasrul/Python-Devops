eddf3566656b233efc6e31b123d059a1aa49ba5c
hasannasrul <hasanali242424@gmail.com>
2023-04-07 22:54:15 +0530

    comment

--- 12_boto3_s3.py
+++ 12_boto3_s3.py
@@ -7,5 +7,5 @@ session = boto3.Session(aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),aw
 s3 = session.client('s3')
 bucket_name = 'hasan-3080-boto'
 response = s3.create_bucket(Bucket=bucket_name)
-
+# response
 print(response)
